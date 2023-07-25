62f6ac31e29b2269ba52b13b51f3775741fcd374
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-07-25 17:11:55 +0530

    Adding Product Brands and types and seeding data

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers;
 
@@ -9,19 +10,20 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
-    public StoreContext _context;
-    public ProductsController(StoreContext context)
+    private readonly IProductRepository _repo;
+
+    public ProductsController(IProductRepository repo)
     {
-        _context = context;
+        _repo = repo;
     }
 
     //GET all products
 
     [HttpGet]
     //[Route("api/products")]
-    public async Task<ActionResult<List<Product>>> GetAllProducts(){
-        var products = await _context.Products.ToListAsync();
-        return products;
+    public async Task<ActionResult<List<Product>>> GetProducts(){
+        var products = await _repo.GetProductsAsync();
+        return Ok(products);
     }
 
     //Get product by Id
@@ -29,8 +31,26 @@ public class ProductsController : ControllerBase
     [Route("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id){
 
-        var product = await _context.Products.FindAsync(id);
-        return product;
+        return await _repo.GetProductByIdAsync(id);
+         
+    }
+
+    [HttpGet]
+    [Route("brands")]
+
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+    { 
+        var brands = await _repo.GetProductBrandsAsync();
+        return Ok(brands);
+    }
+
+    [HttpGet]
+    [Route("types")]
+
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+        var types = await _repo.GetProductTypesAsync();
+        return Ok(types);
     }
 
 
