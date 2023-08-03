65e1c3966dc749fdb0f8fb2c14ac065fbf502293
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-08-03 08:06:35 +0530

    Fixing decimal Price issue of SQLite

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -20,6 +20,22 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //To convert price decimal into double as SQLte does not support decimal types.
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite") 
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties) {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            
+            }
+
+
         }
     }
 
