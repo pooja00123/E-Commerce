7a82dc3c54a57de46c5b3b79002df3af06fba78a
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-08-08 10:26:51 +0530

    Adding product detail page

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -13,6 +13,10 @@ export class ShopService {
   baseUrl = 'https://localhost:7035/api/'
   constructor(private http: HttpClient) { }
 
+  getProduct(id: number){
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getProducts(shopParams : ShopParams){
     let params = new HttpParams();
 
