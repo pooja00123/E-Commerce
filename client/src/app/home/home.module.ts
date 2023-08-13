392c2f2f6870c44a3fe83784c97ff0ebeebbb98a
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-08-13 11:34:03 +0530

    Adding home page content.

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
+
 
 
 
@@ -9,7 +11,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomeComponent
