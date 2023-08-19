e397ab25c5d3ae45b9d6f0ecc6baaa4e080c882c
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-08-19 23:34:20 +0530

    Adding an item to the basket in component method.

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
-import { Basket } from '../shared/models/basket';
+import { Basket, BasketItem } from '../shared/models/basket';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +29,43 @@ export class BasketService {
 
   getCurrentBasketValue(){
     return this.basketSource.value;
+  }
+
+  addItemToBasket(item: Product, quantity =1){
+    const itemToAdd = this.mapProductItemtobasketItem(item);
+    // If current basket value is null then we have to create a basket.
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addorUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  private addorUpdateItem(items: BasketItem[], itemToAdd : BasketItem, quantity: number): BasketItem[] {
+    const item = items.find(x => x.id === itemToAdd.id);
+    if (item) item.quantity += quantity;
+    else {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    }
+    return items;
+  }
+
+  private createBasket(): Basket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  // The product on which we click to add has different properties than the basket item that we 
+  // are storing as basket item in redis database. So we need to map product to basket item. 
+  private mapProductItemtobasketItem(item: Product): BasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity: 0,
+      pictureUrl: item.pictureUrl,
+      brand: item.productBrand,
+      type : item.productType
+    }
   }
 }
