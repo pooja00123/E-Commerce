6e782135a2e0f69d792d3f61e46f7fefa209c466
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-08-25 23:30:01 +0530

    Adding Identity DbContext.

---
+++ API/Extensions/IdentityServicesExtension.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class IdentityServicesExtension
+    {
+        public static IServiceCollection AddIdentityServices (this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+            return services;
+        }
+        
+    }
+}
