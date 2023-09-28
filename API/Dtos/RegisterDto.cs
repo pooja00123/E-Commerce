28877ae209ea16a4d3ee9b7abf8a20ae6d618de2
Pooja Chaudhary <poojachaudhary0119@gmail.com>
2023-09-28 22:36:33 +0530

    Validation

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage ="Password must have 1 uppercase, 1 lowercase, 1 special char, 1 digit and minimum length is 6")]
         public string Password { get; set; }
     }
 }
